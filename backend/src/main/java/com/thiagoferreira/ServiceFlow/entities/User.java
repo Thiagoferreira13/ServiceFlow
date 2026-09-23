3367bf09268139e9388978fa7e6647f8c5acbd79
Thiago Ferreira <thferreira147@gmail.com>
2026-09-23 18:05:54 -0300

    H2 database, test profile, JPA

--- backend/src/main/java/com/thiagoferreira/ServiceFlow/entities/User.java
+++ backend/src/main/java/com/thiagoferreira/ServiceFlow/entities/User.java
@@ -5,13 +5,21 @@ import java.time.Instant;
 
 import com.thiagoferreira.ServiceFlow.enums.Role;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
  
-
+@Entity
+@Table(name = "tb_user") 
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  
     private Long id;
     private String name;
     private String email;
@@ -99,6 +107,43 @@ public class User implements Serializable {
 
     public void setRole(Role role) {
         this.role = role;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        return true;
     }
 
     
